fa42112667f913ef28439f5f5b6064707a34d2ce
Niorlys <niorlysernestoc@gmail.com>
2024-12-07 00:25:17 -0500

    Fixed settings

--- pms/settings.py
+++ pms/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from util.constants import Environment
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-bd!jzce4hap)5b+&17=ccow=hovh9u+s=vshj0_y!xh+$1nlsy"
+SECRET_KEY = os.getenv(Environment.SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "product",
 ]
 
 MIDDLEWARE = [
